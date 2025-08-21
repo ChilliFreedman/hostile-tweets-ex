d029a8ded89d4044b9bf5e88258f96bc93a4d072
chili <ymf21971@gmail.com>
2025-08-21 15:36:23 +0300

    finish func add_new_column_emotion

--- app/processor.py
+++ app/processor.py
@@ -1,4 +1,6 @@
 from pandas.core.interchange.dataframe_protocol import DataFrame
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from collections import Counter
 
 
@@ -7,12 +9,13 @@ class Processor:
         self.df = df
         self.new_df = df.copy()
 
+
     def add_new_column_rarest_word(self):
         list_rarest_word = []
         col_text = self.new_df["Text"]
 
-        for aaa in col_text:
-            count = Counter(aaa.split(" "))
+        for field_text in col_text:
+            count = Counter(field_text.split(" "))
             res = [word for word in count if count[word] == min(count.values())]
             list_rarest_word.append(res[0])
 
@@ -20,6 +23,28 @@ class Processor:
         self.new_df["rarest_word"] = list_rarest_word
         #print(self.new_df)
         return  self.new_df
+
+    def add_new_column_emotion(self):
+        nltk.download('vader_lexicon')  # Compute sentiment labels
+        emotion_list = []
+        col_text = self.new_df["Text"]
+        for field_text in col_text:
+            tweet = field_text
+            score = SentimentIntensityAnalyzer().polarity_scores(tweet)
+            if 1 >= score['compound'] >= 0.5:
+                emot = "positive"
+            elif  0.49 >= score['compound'] >= -0.49:
+                emot = "neutral"
+            else:
+                emot = "negative"
+
+            emotion_list.append(emot)
+        #print(len(emotion_list))
+        self.new_df["emotion"] = emotion_list
+        print(self.new_df)
+        #return self.new_df
+
+
 
 
 
@@ -33,5 +58,7 @@ if __name__ == "__main__":
     dal = DAL(connector)
     df = dal.get_mongoDB_as_df()
     processor = Processor(df)
-    print(processor.add_new_column_rarest_word())
+    processor.add_new_column_rarest_word()
+    processor.add_new_column_emotion()
+
 
--- scripts/commands.bat
+++ scripts/commands.bat
@@ -3,3 +3,6 @@ pip install pymongo
 
 #
 pip install pandas
+
+#
+pip install nltk
